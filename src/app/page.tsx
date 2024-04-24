a37768cacf47ada9919e1d607bf32ab56a8a99c9
Paulo Fernando <paulofernando78@gmail.com>
2024-04-24 20:53:10 -0300

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1>Welcome to Daily Basis English Course</h1>
       <p>
           <b>
-            Hi! Welcome to your student's website. Here, you'll find a lot of interesting materials. Please, use it wisely.
+            Hi! Welcome to your student`&apos;`s website. Here, you{"'"}ll find a lot of interesting materials. Please, use it wisely.
           </b>
         </p>
       <div>
