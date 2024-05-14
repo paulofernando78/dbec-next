3a199901eba5d49418701fe02ab90e5c66517b67
Paulo Fernando <paulofernando78@gmail.com>
2024-05-14 19:03:26 -0300

    up

--- src/app/courses/placement-test/page.tsx
+++ src/app/courses/placement-test/page.tsx
@@ -1,16 +1,45 @@
-import { Card } from "@/components/Card/Card";
-import { Whiteboard } from "@/components/Whiteboard/Whiteboard"
+import { Whiteboard } from "@/components/Whiteboard/Whiteboard";
+import styles from "./PlacementTest.module.css";
 
-export default function placementTest () {
+interface PlacementTestProps {
+  levels: string[]
+  taskNumber: string
+  taskType: string
+  languageFocus: string
+  examples: string
+}
+
+export default function PlacementTest({levels, taskNumber, taskType, languageFocus, examples}: PlacementTestProps) {
   return (
     <>
-      <Whiteboard
-      title="Courses"
-      subtitle="Placement Test"
-      />
-      <Card>
-        <p>teste</p>
-      </Card>
+      <Whiteboard title="Courses" subtitle="Placement Test" />
+      {levels.map((level, index) => (
+      <div className="line-break" key={index}>
+        <div className={styles["main-container"]}>
+            <p className={styles["main-container-level"]}>{level}</p>
+          <div>
+          </div>
+          <div className={styles["inner-container"]}>
+            <div>
+              <p className={styles["bold"]}>TASK #</p>
+              {taskNumber}
+            </div>
+            <div>
+              <p className={styles["bold"]}>TASK TYPE</p>
+              {taskType}
+            </div>
+            <div>
+              <p className={styles["bold"]}>LANGUAGE FOCUS</p>
+              <p>{languageFocus}</p>
+            </div>
+            <div>
+              <p className={styles["bold"]}>EXAMPLES</p>
+              <p>{examples}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      ))}
     </>
   );
 }
