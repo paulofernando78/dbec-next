6b321937df7e758414d999c5d3efd46005acb266
Paulo Fernando <paulofernando78@gmail.com>
2024-05-17 22:19:04 -0300

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-        async headers() {
-          return [
-            {
-              // This will match all routes
-              source: '/:path',
-              headers: [
-                {
-                  key: 'Access-Control-Allow-Credentials',
-                  value: 'true',
-                },
-                {
-                  key: 'Access-Control-Allow-Origin',
-                  value: '', // You can replace '*' with specific origin(s) if needed
-                },
-                {
-                  key: 'Access-Control-Allow-Methods',
-                  value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-                },
-                {
-                  key: 'Access-Control-Allow-Headers',
-                  value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-                },
-              ],
-            },
-          ];
-        },
-      };
+    //     async headers() {
+    //       return [
+    //         {
+    //           // This will match all routes
+    //           source: '/:path',
+    //           headers: [
+    //             {
+    //               key: 'Access-Control-Allow-Credentials',
+    //               value: 'true',
+    //             },
+    //             {
+    //               key: 'Access-Control-Allow-Origin',
+    //               value: '', // You can replace '*' with specific origin(s) if needed
+    //             },
+    //             {
+    //               key: 'Access-Control-Allow-Methods',
+    //               value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    //             },
+    //             {
+    //               key: 'Access-Control-Allow-Headers',
+    //               value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    //             },
+    //           ],
+    //         },
+    //       ];
+    //     },
+    //   };
+}
 
 export default nextConfig;
--- src/utils/searchWords.ts
+++ src/utils/searchWords.ts
@@ -1,21 +1,21 @@
 import { IDictionary } from "../../interfaces";
+import Dictionary from "../../public/assets/data/dictionary.json";
 
-export async function searchWords(word: string) {
-    try {
-      // Faz e espera a requisição
-      const response = await fetch(
-        "http://localhost:3000/assets/data/dictionary.json"
-      );
-      // Converte em formato JSON e Faz a Tipagem do Retorno
-      const data = (await response.json()) as IDictionary[];
-      // Filtra o dicionário para pegar as palavras que contêm a palavra inserida
-      const wordsFound = data.filter((dict) => {
-        // converte para letra mínuscula para melhor compatibilidade
-        return dict.keyword.toLowerCase().includes(word.toLowerCase());
-      });
-      // Retorna o dicionário filtrado
-      return wordsFound;
-    } catch (error: any) {
-      console.error(error.message);
-    }
+export async function searchWords(word: string): Promise<IDictionary[]> {
+  try {
+    // Directly use the imported dictionary data
+    const data: IDictionary[] = Dictionary as IDictionary[];
+
+    // Filtra o dicionário para pegar as palavras que contêm a palavra inserida
+    const wordsFound = data.filter((dict) => {
+      // converte para letra mínuscula para melhor compatibilidade
+      return dict.keyword.toLowerCase().includes(word.toLowerCase());
+    });
+
+    // Retorna o dicionário filtrado
+    return wordsFound;
+  } catch (error: any) {
+    console.error(error.message);
+    return [];
   }
+}
