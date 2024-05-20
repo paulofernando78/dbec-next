bd818149ee1dba549c33fbfd3597ae9d3334d2fc
Paulo Fernando <paulofernando78@gmail.com>
2024-05-20 19:00:57 -0300

    wip

--- src/app/extras/grammar/page.tsx
+++ src/app/extras/grammar/page.tsx
@@ -5,10 +5,43 @@ const descriptions = [{
   label: "Present",
   contents: [
     {
-      content: "Yeahh!!!",
+      content: "...",
       linkLabel: "Unit 1 - am/is/are",
       link: "/extras/grammar/uni-1"
-    }
+    },
+    {
+      linkLabel: "Unit 2 - am/is/are (questons)",
+      link: "/extras/grammar/uni-"
+    },
+    {
+      linkLabel: "Unit 3 - ...",
+      link: "/extras/grammar/uni-"
+    },
+    {
+      linkLabel: "Unit 4 - ...",
+      link: "/extras/grammar/uni-"
+    },
+    {
+      linkLabel: "Unit 5 - ...",
+      link: "/extras/grammar/uni-"
+    },
+    {
+      linkLabel: "Unit 6 - ...",
+      link: "/extras/grammar/uni-"
+    },
+    {
+      linkLabel: "Unit 7 - ...",
+      link: "/extras/grammar/uni-"
+    },
+    {
+      linkLabel: "Unit 8 - ...",
+      link: "/extras/grammar/uni-"
+    },
+    {
+      linkLabel: "Unit 9 - ...",
+      link: "/extras/grammar/uni-"
+    },
+
   ] 
 }];
 
--- src/components/DescriptionCard/DescriptionCard.tsx
+++ src/components/DescriptionCard/DescriptionCard.tsx
@@ -2,40 +2,44 @@ import Link from "next/link";
 import style from "./DescriptionCard.module.css";
 
 interface DescriptionCardProps {
-descriptions: Description[];
+  descriptions: Description[];
 }
 
 interface Description {
-  label: string
-  contents: Content[]
+  label: string;
+  contents: Content[];
 }
 
 interface Content {
-  content: string;
+  content?: string;
   link?: string;
-  linkLabel?: string
+  linkLabel?: string;
 }
 
-export const DescriptionCard = ({ descriptions
-}: DescriptionCardProps) => {
+export const DescriptionCard = ({ descriptions }: DescriptionCardProps) => {
   return (
     <>
-    {descriptions.map((description, descIndex) => (
-      <div key={descIndex} className={style["card-description"]}>
-        <div className={style["card-description-label"]}>
-          <p>{description.label}</p>
-        </div>
+      {descriptions.map((description, descIndex) => (
+        <div key={descIndex} className={style["card-description"]}>
+          <div className={style["card-description-label"]}>
+            <p>{description.label}</p>
+          </div>
 
-{description.contents.map((content, contentIndex) => (
-  <div key={contentIndex} className={style["card-description-content"]}>
-    <p>{content.content}</p>
-    {content.link && content.linkLabel && (
-    <Link href={content.link}><p>{content.linkLabel}</p></Link>
-    )}
-  </div>
-))}
-</div>
-    ))}
+          {description.contents.map((content, contentIndex) => (
+            <div
+              key={contentIndex}
+              className={style["card-description-content"]}
+            >
+              <p>{content.content}</p>
+              {content.link && content.linkLabel && (
+                <Link href={content.link}>
+                  <p>{content.linkLabel}</p>
+                </Link>
+              )}
+            </div>
+          ))}
+        </div>
+      ))}
     </>
   );
 };
