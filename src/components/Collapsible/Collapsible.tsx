0387a5c62b44a276c9338fe336c786ad5f717853
Paulo Fernando <paulofernando78@gmail.com>
2024-05-20 00:18:10 -0300

    up

--- src/components/Collapsible/Collapsible.tsx
+++ src/components/Collapsible/Collapsible.tsx
@@ -1,9 +1,14 @@
 "use client";
 import { useState } from "react";
-import { Card } from "../Card/Card";
+import Image from "next/image";
 
+// CSS
+import styles from "./styles.module.css"
+
+// Images Icons
 import plusIcon from "@/img/icon/plus.png"
 import minusIcon from "@/img/icon/minus.png"
+import { Card } from "../Card/Card";
 
 interface CollapsibleProps {
   label?: string;
@@ -19,14 +24,14 @@ export const Collapsible = ({ label, labelBold, children }: CollapsibleProps) =>
   };
   return (
     <>
-      <Card>
-        <span onClick={toggleCollapse} className="cursor-pointer">
-        {isOpen ? "-" : "+"}
-          {label}
-          <b>{labelBold}</b>
-        </span>
-        <span>{isOpen && <p className="block margin-top">{children}</p>}</span>
-      </Card>
+        <Card>
+          <span onClick={toggleCollapse} className="cursor-pointer">
+          <Image src={isOpen ? minusIcon : plusIcon} alt="Icons" className={styles["plus-minus-icons"]}/>
+            <p className={`${"inline-block user-select-none margin-left user-select-none"} ${styles["label"]}`}>{label}</p>
+            <p className={`${"inline-block user-select-none margin-left user-select-none"} ${styles["label"]}`}><b>{labelBold}</b></p>
+          </span>
+          <span>{isOpen && <p className="block margin-top">{children}</p>}</span>
+        </Card>
     </>
   );
 };
