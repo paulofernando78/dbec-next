423f882e9c31417c0689028dea6f78c62bf0bbf7
Paulo Fernando <paulofernando78@gmail.com>
2024-06-06 01:47:41 -0300

    wip

--- src/components/Extras/Pronunciation/Letters/LetterEY/index.tsx
+++ src/components/Extras/Pronunciation/Letters/LetterEY/index.tsx
@@ -9,11 +9,11 @@ export const LettersEY = () => {
           <p className="bold">Letter ey</p>
         </Card>
         <div className="flex-8px-center-wrap">
-          <DictionaryCard audioSrc="/c/chimney.mp3" label="chimney" phonetics="/ˈtʃɪm.ni/"/>
-          <DictionaryCard audioSrc="/d/donkey.mp3" label="donkey" phonetics="/ˈdɑːŋ.ki/"/>
-          <DictionaryCard audioSrc="/k/key.mp3" label="key" phonetics="/kiː/"/>
-          <DictionaryCard audioSrc="/m/money.mp3" label="money" phonetics="/ˈmʌn.i/"/>
-          <DictionaryCard audioSrc="/m/monkey.mp3" label="monkey" phonetics="/ˈmʌŋ.ki/"/>
+          <DictionaryCard audioSrc="/c/chimney.mp3" label="chimn<span class='underline'>ey</span>" phonetics="/ˈtʃɪm.ni/"/>
+          <DictionaryCard audioSrc="/d/donkey.mp3" label="donk<span class='underline'>ey</span>" phonetics="/ˈdɑːŋ.ki/"/>
+          <DictionaryCard audioSrc="/k/key.mp3" label="k<span class='underline'>ey</span>" phonetics="/kiː/"/>
+          <DictionaryCard audioSrc="/m/money.mp3" label="mon<span class='underline'>ey</span>" phonetics="/ˈmʌn.i/"/>
+          <DictionaryCard audioSrc="/m/monkey.mp3" label="monk<span class='underline'>ey</span>" phonetics="/ˈmʌŋ.ki/"/>
         </div>
       </div>
     </>
