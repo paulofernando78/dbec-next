af86b5af6b08b7abf78ad7f30defb092810ed674
Paulo Fernando <paulofernando78@gmail.com>
2024-07-24 20:04:34 -0300

    wip

--- src/components/Cards/Flipped/index.tsx
+++ src/components/Cards/Flipped/index.tsx
@@ -1,15 +1,22 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
-import {cat1, questionMarkGif} from "@/img/index"
+import { questionMarkGif } from "@/img/index";
 
 export const FlipCard = () => {
   return (
-    <div className={`img-border ${styles["container"]}`}>
-      <div className={styles["card"]}>
-        <div className={styles["front"]}>Front
-            <Image src={questionMarkGif} alt="..." className={styles["front-image"]}/>
+    <div className={styles["container"]}>
+      <div className={`img-border ${styles["card"]}`}>
+        <div className={styles["front"]}>
+          <Image
+            src={questionMarkGif}
+            alt="Question mark gif"
+            className={styles["front-image"]}
+          />
+          <p className={styles["front-text"]}>What's the past of ask?</p>
         </div>
-        <div className={styles["back"]}>Back</div>
+        <div className={styles["back"]}>
+          <p>Back</p>
+        </div>
       </div>
     </div>
   );
