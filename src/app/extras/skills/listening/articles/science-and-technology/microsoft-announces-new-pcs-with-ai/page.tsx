6d816657564e51df31cdddc9badd4691847f5232
Paulo Fernando <paulofernando78@gmail.com>
2024-07-30 08:34:55 -0300

    wip

--- src/app/extras/skills/listening/articles/science-and-technology/microsoft-announces-new-pcs-with-ai/page.tsx
+++ src/app/extras/skills/listening/articles/science-and-technology/microsoft-announces-new-pcs-with-ai/page.tsx
@@ -92,7 +92,7 @@ const preVocabularies: PreVocabulary[] = [
     component: (props) => <DictionaryCard {...props} />,
     componentProps: {
       audioSrc: "/c/compelling.mp3",
-      keyword: "compelling ",
+      keyword: "compel ",
       label: "compelling ",
       phonetics: "/kəmˈpel.ɪŋ/",
     },
