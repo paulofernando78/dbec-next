bbfd9825a29e2a555f31ff1fac0446492ae219f7
Paulo Fernando <paulofernando78@gmail.com>
2024-11-16 19:05:09 -0300

    wip

--- src/components/Templates/Specific-Purposes/Business/Sbella/Index.tsx
+++ src/components/Templates/Specific-Purposes/Business/Sbella/Index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Whiteboard } from "@/components/Whiteboard";
-import { AudioPlayer, Card, Radio } from "@/components";
+import { AudioPlayer, Card, DictionaryCard, Radio } from "@/components";
 import { SbelaaLessonTemplateProps } from "@/types/materials/specific-purposes/business/sbelaa/sbelaa";
 
 export const SbellaLessonTemplate = ({ data }: SbelaaLessonTemplateProps) => {
@@ -13,7 +13,9 @@ export const SbellaLessonTemplate = ({ data }: SbelaaLessonTemplateProps) => {
         descriptions={data.descriptions}
       />
       <div className="line-break">
-        <AudioPlayer audioSrc={data.audioSrc} />
+        <div className="audio-fixed">
+          <AudioPlayer audioSrc={data.audioSrc} />
+        </div>
         <Card>
           <p>
             <b>{data.lessonTitle}</b>
@@ -21,16 +23,32 @@ export const SbellaLessonTemplate = ({ data }: SbelaaLessonTemplateProps) => {
           <p>{data.lessonDescription}</p>
         </Card>
         {data.paragraphs.map((paragraph, index) => (
-          <p key={index}>
-            <b>{paragraph.speaker}</b> {paragraph.text}
-          </p>
+          <div>
+            <p key={index} className="display-inline">
+              <b>{paragraph.speaker}:</b>
+            </p>{" "}
+            {paragraph.texts.map((text, textIndex) => (
+              <div key={textIndex} className="display-inline">
+                <div style={{ display: "inline" }}>
+                  {text.vocabulary && (
+                    <DictionaryCard
+                      keyword={text.keyword}
+                      label={text.vocabulary}
+                      audioSrc={text.audioSrc}
+                    />
+                  )}
+                </div>{" "}
+                <p className="display-inline">{text.text} </p>
+              </div>
+            ))}
+          </div>
         ))}
         <Card bgColor="lightgrey">
           <p>
             <b>Exercises</b>
           </p>
         </Card>
-        {data.radioExercises && data.radioExercises.length > 0 &&(
+        {data.radioExercises && data.radioExercises.length > 0 && (
           <Radio questions={data.radioExercises} />
         )}
       </div>
--- src/types/materials/specific-purposes/business/sbelaa/sbelaa.ts
+++ src/types/materials/specific-purposes/business/sbelaa/sbelaa.ts
@@ -1,25 +1,33 @@
 interface RadioExercise {
-    title?: string;
-    question: string;
-    options: { label: string; isCorrect: boolean }[];
-  }
-  
-  interface Paragraph {
-    speaker: string;
-    text: string;
-  }
-  
-  export interface LessonData {
-    title: string;
-    subtitle: string;
-    descriptions: string[];
-    audioSrc: string;
-    lessonTitle: string;
-    lessonDescription: string;
-    paragraphs: Paragraph[];
-    radioExercises: RadioExercise[];
-  }
-  
-  export interface SbelaaLessonTemplateProps {
-    data: LessonData;
-  }
+  title?: string;
+  question: string;
+  options: { label: string; isCorrect: boolean }[];
+}
+
+interface Text {
+  keyword: string;
+  vocabulary: string;
+  text: string;
+  audioSrc: string;
+}
+
+interface Paragraph {
+  speaker: string;
+  text: string;
+  texts: Text[];
+}
+
+export interface LessonData {
+  title: string;
+  subtitle: string;
+  descriptions: string[];
+  audioSrc: string;
+  lessonTitle: string;
+  lessonDescription: string;
+  paragraphs: Paragraph[];
+  radioExercises: RadioExercise[];
+}
+
+export interface SbelaaLessonTemplateProps {
+  data: LessonData;
+}
