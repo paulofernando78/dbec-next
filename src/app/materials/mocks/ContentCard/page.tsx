258fc53848111dae5f1c81583fec215d79357cef
Paulo Fernando <paulofernando78@gmail.com>
2024-11-29 04:35:59 -0300

    wip

--- src/app/materials/mocks/ContentCard/page.tsx
+++ src/app/materials/mocks/ContentCard/page.tsx
@@ -1,34 +1,49 @@
+"use client";
+import { useEffect, useState } from "react";
+
 import { ContentCard } from "@/components/Molecules/Cards/ContentCard";
+import {
+  MainContent,
+  Whiteboard,
+} from "@/components/Molecules/Cards/ContentCard/type";
 
-const contents = [
-  {
-    cardContents: [
-      {
-        bgColor: "black",
-        textColor: "white",
-        cardLabel: "...",
-        contents: [
-          {
-            content: "...",
-          },
-          {
-            link: "...",
-            linkLabel: "Test ... ",
-          },
-          {
-            globeLink: "...",
-            globeLabel: "Test ... ",
-          },
-          {
-            checkboxLink: "...",
-            checkboxLabel: "Test ... ",
-          },
-        ],
-      },
-    ],
-  },
-];
+export default function ContentCardTemplate() {
+  const [contentData, setContentData] = useState<{
+    whiteboard?: Whiteboard;
+    contents: MainContent[];
+  }>({
+    whiteboard: undefined,
+    contents: [],
+  });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-export default function ContentCardComponent() {
-  return <ContentCard contents={contents} />;
+  const contentCardTemplate =
+    "/assets/data/materials/templates/contentCard.json";
+
+  useEffect(() => {
+    fetch(contentCardTemplate)
+      .then((response) => {
+        if (!response.ok) throw new Error("Error loading JSON");
+        return response.json();
+      })
+      .then((data: { whiteboard?: Whiteboard; contents: MainContent[] }) => {
+        setContentData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error loading lesson data.</p>;
+
+  return (
+    <>
+      <ContentCard contentData={contentData} />
+    </>
+  );
 }
