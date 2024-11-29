e539bf3e95d048decea88fa3e8f783c838ba596d
Paulo Fernando <paulofernando78@gmail.com>
2024-11-29 04:27:33 -0300

    wip

--- src/app/materials/extras/skills/listening/articles/science-and-technology/page.tsx
+++ src/app/materials/extras/skills/listening/articles/science-and-technology/page.tsx
@@ -1,53 +1,49 @@
+"use client";
+import { useEffect, useState } from "react";
+
 import { ContentCard } from "@/components/Molecules/Cards/ContentCard";
+import {
+  MainContent,
+  Whiteboard,
+} from "@/components/Molecules/Cards/ContentCard/type";
 
-const whiteboard = {
-  title: "Listening",
-  subtitle: "Articles",
-  descriptions: ["Science & Technology"],
-};
+export default function ExtrasSkillsListeningArticlesScienceAndTechnology() {
+  const [contentData, setContentData] = useState<{
+    whiteboard?: Whiteboard;
+    contents: MainContent[];
+  }>({
+    whiteboard: undefined,
+    contents: [],
+  });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-const contents = [
-  {
-    cardContents: [
-      {
-        bgColor: "black",
-        textColor: "white",
-        cardLabel: "By date",
-        contents: [
-          // Chinese Rocket Breaks Up in Space, Releasing Hundreds of Debris Pieces (August 13, 2024)
-          {
-            link: "/extras/skills/listening/articles/science-and-technology/chinese-rocket-breaks-up-in-space",
-            linkLabel:
-              "Chinese Rocket Breaks Up in Space, Releasing Hundreds of Debris Pieces (August 13, 2024)",
-          },
-          // Microsoft Announces New PCs with Built-in AI (May 23, 2024)
-          {
-            link: "/extras/skills/listening/articles/science-and-technology/microsoft-announces-new-pcs-with-ai",
-            linkLabel:
-              "Microsoft Announces New PCs with Built-in AI (May 23, 2024)",
-          },
-          // Florida Approves Law Banning Social Media for Children under 14 (March 27, 2024)
-          {
-            link: "/extras/skills/listening/articles/science-and-technology/florida-approves-law-banning-social-media",
-            linkLabel:
-              "Florida Approves Law Banning Social Media for Children under 14 (March 27, 2024)",
-          },
-          // Israeli Scientists Announce Creation of Embryo Model in Lab (September 11, 2023)
-          {
-            link: "/extras/skills/listening/articles/science-and-technology/israeli-scientists-announce-creation-of-embryo-model-in-lab",
-            linkLabel:
-              "Israeli Scientists Announce Creation of Embryo Model in Lab (September 11, 2023)",
-          },
-        ],
-      },
-    ],
-  },
-];
+  const extrasSkillsListeningArticlesScienceAndTechnology =
+    "/assets/data/materials/extras/skills/listening/articles/science-and-technology/contents.json";
 
-export default function ScienceAndTechnologyArticles() {
+  useEffect(() => {
+    fetch(extrasSkillsListeningArticlesScienceAndTechnology)
+      .then((response) => {
+        if (!response.ok) throw new Error("Error loading JSON");
+        return response.json();
+      })
+      .then((data: { whiteboard?: Whiteboard; contents: MainContent[] }) => {
+        setContentData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error loading lesson data.</p>;
+
   return (
     <>
-      <ContentCard whiteboard={whiteboard} contents={contents} />
+      <ContentCard contentData={contentData} />
     </>
   );
 }
