8e69dd78fc8688014bd739a11dc8f5b749af5d6a
Paulo Fernando <paulofernando78@gmail.com>
2024-12-03 23:15:32 -0300

    wip

--- src/components/Molecules/Inputs/Login/index.tsx
+++ src/components/Molecules/Inputs/Login/index.tsx
@@ -5,13 +5,11 @@ import { Card } from "@/components";
 
 export const Login = () => {
   return (
-    <Card>
       <form>
         <p>email: </p>
         <input type="text" id="email" />
         <p>password: </p>
         <input type="password" id="password" />
       </form>
-    </Card>
   );
 };
