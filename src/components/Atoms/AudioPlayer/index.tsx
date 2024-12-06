b9649ee1bb12c6224dc639c4af6528e85b5dadf4
Paulo Fernando <paulofernando78@gmail.com>
2024-12-05 23:25:27 -0300

    wip

--- src/components/Atoms/AudioPlayer/index.tsx
+++ src/components/Atoms/AudioPlayer/index.tsx
@@ -1,8 +1,8 @@
 import styles from "./style.module.css";
 import { AudioPlayerProps } from "./types";
 
-
-
 export const AudioPlayer: React.FC<AudioPlayerProps> = ({ audioSrc }) => {
-  return <audio controls src={audioSrc || ""} className={styles["audio"]} />;
+  const baseAudioSrc = "/assets/audio";
+  const fullAudioSrc = audioSrc ? `${baseAudioSrc}${audioSrc}` : "";
+  return <audio controls src={fullAudioSrc} className={styles["audio"]} />;
 };
