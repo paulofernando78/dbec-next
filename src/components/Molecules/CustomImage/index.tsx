bddbf8358e5cc89793ae0c15b45e5d6907ff2251
Paulo Fernando <paulofernando78@gmail.com>
2024-12-06 15:56:30 -0300

    wip

--- src/components/Molecules/CustomImage/index.tsx
+++ src/components/Molecules/CustomImage/index.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import { CustomImageProps } from "./type";
 
 export const CustomImage = ({ customImg, customImgAlt }: CustomImageProps) => {
+  const baseImgScr = "/assets/img"
   return (
     <Image
       className="img-border"
-      src={customImg}
+      src={`${baseImgScr}${customImg}`}
       alt={customImgAlt}
       width={1000}
       height={1000}
