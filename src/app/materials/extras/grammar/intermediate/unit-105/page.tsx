82aab7dbc45fb0146d5a92591b77cbc8e0b22d19
Paulo Silva <paulosilva@Paulos-Mac-mini.local>
2024-12-24 01:19:46 -0300

    added grammar intermediate folders

---
+++ src/app/materials/extras/grammar/intermediate/unit-105/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+// Hooks
+import { useEffect, useState } from "react";
+
+// Components
+import { LessonTemplate } from "@/components/Templates/LessonData/Index";
+
+export default function ExtrasGrammarIntermediateUnit105() {
+  const [lessonData, setLessonData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const CONTENTS_JSON_PATH = "/assets/data/materials/extras/grammar/intermediate/unit-105.json";
+
+  useEffect(() => {
+    fetch(CONTENTS_JSON_PATH)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch lesson data");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setLessonData(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error loading lesson data.</p>;
+
+  return (
+    <LessonTemplate lessonData={lessonData} isUnderConstruction={true} />
+  );
+}
